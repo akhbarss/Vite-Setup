07725becafb783105419d85f64633de5998498ea
abay bay <akbar25005@gmail.com>
2023-10-07 20:42:42 +0700

    add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Link,
+} from "react-router-dom"
+import "./App.css"
 
-function App() {
-  const [count, setCount] = useState(0)
+const Navbar = () => (
+  <nav 
+  style={{ 
+    display: "flex",
+    position: 'fixed',
+    top: 0,
+    width: "100%",
+    height: "13vh",
+    backgroundColor: "black",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: "3rem"
+   }}
+  >
+    <Link to={"/"}>Dashboard</Link>
+    <Link to={"/about"}>About</Link>
+  </nav>
+)
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <RouterProvider router={createBrowserRouter([
+      {
+        path: "/",
+        Component: () => (
+          <div style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "100%"
+          }}>
+            <Navbar />
+            <h1>DASHBOARD</h1>
+          </div>
+        )
+      },
+      {
+        path: "/about",
+        Component: () => (
+          <div style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "100%"
+          }}>
+            <Navbar />
+            <h1>ABOUT</h1>
+          </div>
+        )
+      },
+    ])} />
   )
 }
 
